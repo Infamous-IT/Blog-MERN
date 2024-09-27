c359d8a86ad15ae46d7608a5c56fd7ad3872ae26
infamous-it <it.dev.23@gmail.com>
2024-09-27 14:12:05 +0300

    Basic config Routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import About from "./pages/About.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import Projects from "./pages/Projects.jsx";
+
 function App() {
 
   return (
     <>
-        <h1 className="text-3xl font-bold underline">Hello React!</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/sign-in" element={<SignIn/>}/>
+                <Route path="/sign-up" element={<SignUp/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/projects" element={<Projects/>}/>
+            </Routes>
+        </BrowserRouter>
     </>
   )
 }
